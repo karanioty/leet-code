e9fe0ed76052db48ba37eefe8a2367780ef5255e
karanioty <karan11052007@gmail.com>
2026-06-22 22:21:03 +0530

    feat:day 1

--- answer.py
+++ answer.py
@@ -82,4 +82,21 @@ class Solution:
                 d=i
                 break
         return d'''
+'''class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def rotate(arr,i,j):
+            while(i<j):
+                arr[i],arr[j]=arr[j],arr[i]
+                i+=1
+                j-=1
+        def call(arr,n,s):
+            rotate(arr,0,n-1)
+            rotate(arr,0,s-1)
+            rotate(arr,s,n-1)
+            return arr
+        if len(nums)>1:
+            call(nums,len(nums),k%len(nums))'''
         
